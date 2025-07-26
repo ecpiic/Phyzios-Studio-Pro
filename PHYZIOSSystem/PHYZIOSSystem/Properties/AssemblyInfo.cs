9e7ef602145a29094cd80901ea04110e79aa4dc3
Atherusti <153565919+ecpiic@users.noreply.github.com>
2025-07-26 16:32:52 +0200

    Add files via upload

---
+++ PHYZIOSSystem/PHYZIOSSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: Guid("6a1964a0-3618-4203-8f3c-3024d559174c")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle("OECasualCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OECasualCSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
