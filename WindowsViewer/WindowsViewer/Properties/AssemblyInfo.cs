404913b6cfccb84ecbac89ae4838ad0e8d11b2f2
Atherusti <153565919+ecpiic@users.noreply.github.com>
2025-07-26 16:14:30 +0200

    Add files via upload

---
+++ WindowsViewer/WindowsViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+[assembly: AssemblyVersion("1.1.17.40108")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTrademark("PHYZIOS Studio Pro Editor")]
+[assembly: Guid("60ff44aa-9476-4ba8-a7e1-c621055ba9f1")]
+[assembly: AssemblyCompany("PHYZIOS,Inc.")]
+[assembly: AssemblyFileVersion("1.1.17.0")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: InternalsVisibleTo("DataChangerTest")]
+[assembly: AssemblyTitle("PHYZIOS Studio Pro Editor")]
+[assembly: AssemblyDescription("Proffesional Editor for PHYZIOS Studio.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("PHYZIOS Studio Pro Editor")]
